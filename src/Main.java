a6493d1ab4f4e0f847f819cbf1dd02f3f84f5259
nikitastrigunkov <demonkurt1995@gmail.com>
2023-02-15 18:53:52 +0300

    API

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.function.BiConsumer;
+import java.util.stream.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
